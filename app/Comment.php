15f58a080f1b70f40f57554eb6c9cddcb1bfaadd
Namal Sirikumara <namal@omobio.net>
2019-03-10 13:12:03 +0530

    Relationship created

--- app/Comment.php
+++ app/Comment.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * Application
+ *
+ * @author       Duminda Namal
+ * @version      $Id: v1.0.0 2019-March-10 Exp $;
+ * @copyright    Copyright &copy; NextDevelopers.
+ */
 
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Post;
+
 class Comment extends Model
 {
-    //
+    
+    public function post(){
+        return $this -> belongsTo(Post::class, 'postId');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * Application
+ *
+ * @author       Duminda Namal
+ * @version      $Id: v1.0.0 2019-March-10 Exp $;
+ * @copyright    Copyright &copy; NextDevelopers.
+ */
 
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Comment;
+
 class Post extends Model
 {
-    //
+    public function comments(){
+        return $this ->hasMany(Comment::class, 'postId'); //pass the foringn key here
+    }
 }
